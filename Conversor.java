820df872ad2d86ef1c027df3b3ed3802fb35fe05
MaiconDequicy <maiconcosta@icomp.ufam.ufam.edu.br>
2024-07-19 08:34:21 -0400

    alteração no codigo de temperatura

--- Conversor.java
+++ Conversor.java
@@ -1,20 +1,24 @@
-
 public class Conversor {
 
     public static double fahrenheitParaCelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5 / 9;
+        System.out.println("Convertendo " + fahrenheit + " F para Celsius...");
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        System.out.println("Resultado: " + celsius + " C");
+        return celsius;
     }
 
     public static double celsiusParaFahrenheit(double celsius) {
-        return (celsius * 9 / 5) + 32;
+        System.out.println("Convertendo " + celsius + " C para Fahrenheit...");
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println("Resultado: " + fahrenheit + " F");
+        return fahrenheit;
     }
 
     public static void main(String[] args) {
         double fahrenheit = 82;
         double celsius = 32;
 
-        System.out.println(fahrenheit + " F eh igual a " + fahrenheitParaCelsius(fahrenheit) + " C");
-        System.out.println(celsius + " C eh igual a " + celsiusParaFahrenheit(celsius) + " F");
-
+        System.out.println(fahrenheit + " F é igual a " + fahrenheitParaCelsius(fahrenheit) + " C");
+        System.out.println(celsius + " C é igual a " + celsiusParaFahrenheit(celsius) + " F");
     }
 }
