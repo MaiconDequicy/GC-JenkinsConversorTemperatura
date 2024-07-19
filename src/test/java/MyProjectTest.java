f86b44cd1d4620ab1f68f1f395f16cddeb5e698f
MaiconDequicy <maiconcosta@icomp.ufam.ufam.edu.br>
2024-07-19 09:21:48 -0400

    vai

--- src/test/java/MyProjectTest.java
+++ src/test/java/MyProjectTest.java
@@ -1,7 +1,11 @@
 package src.test.java;
 
+import static org.junit.jupiter.api.Assertions.assertTrue; // Import static para assertTrue
+
 public class MyProjectTest {
+
+    @Test
     public void testExample() {
-        assertTrue(true); // Teste simples
+        assertTrue(true); // Teste simples utilizando o assertTrue do JUnit
     }
 }
