e60a3d78c2f33e29cbf2a43cb1ccc37abbbf6ce4
MaiconDequicy <maiconcosta@icomp.ufam.ufam.edu.br>
2024-07-19 09:13:38 -0400

    vai

--- src/test/java/ConversorTest.java
+++ src/test/java/ConversorTest.java
@@ -1,4 +1,4 @@
-package src.test;
+package src.test.java;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
--- src/test/java/MyProjectTest.java
+++ src/test/java/MyProjectTest.java
@@ -1,4 +1,4 @@
-package src.test;
+package src.test.java;
 
 public class MyProjectTest {
     public void testExample() {
