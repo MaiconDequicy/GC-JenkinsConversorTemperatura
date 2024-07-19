f1da79b5f5ca460f14290b09c47cffed38b2d0f3
MaiconDequicy <maiconcosta@icomp.ufam.ufam.edu.br>
2024-07-19 08:31:17 -0400

    inclusão de casos de teste

---
+++ ConversorTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ConversorTest {
+
+    @Test
+    public void testFahrenheitParaCelsius() {
+        assertEquals(0, Conversor.fahrenheitParaCelsius(32), 0.001);
+        assertEquals(100, Conversor.fahrenheitParaCelsius(212), 0.001);
+    }
+
+    @Test
+    public void testCelsiusParaFahrenheit() {
+        assertEquals(32, Conversor.celsiusParaFahrenheit(0), 0.001);
+        assertEquals(212, Conversor.celsiusParaFahrenheit(100), 0.001);
+    }
+}
